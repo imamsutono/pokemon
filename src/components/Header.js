ba78f95835cec0a9893fd33fd3e8ec0bed0ea218
Stephen Daniel <stephendaniel60@gmail.com>
2021-01-15 17:25:48 +0700

    Add home and compare menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => (
 	<nav className="navbar" role="navigation" aria-label="main navigation">
 	  <div className="navbar-brand">
-	    <a className="navbar-item" href="https://warungpintar.co.id">
+	    <a className="navbar-item" href="https://warungpintar.co.id" target="_blank">
 	      <img src={require('../images/pokeball.svg').default} width="112" height="28" alt="Pokemon Pintar" />
 	      <strong>Pokemon Pintar</strong>
 	    </a>
@@ -13,6 +14,13 @@ const Header = () => (
 	      <span aria-hidden="true"></span>
 	      <span aria-hidden="true"></span>
 	    </button>
+
+			<div id="navbarBasicExample" class="navbar-menu">
+				<div class="navbar-start">
+					<Link className="navbar-item" to="/">Home</Link>
+					<Link className="navbar-item" to="/compare">Compare</Link>
+				</div>
+			</div>
 	  </div>
 	</nav>
 );
