a61e3a30ff04e5b0979e1f3e14167a60222fe931
Stephen Daniel <stephendaniel60@gmail.com>
2021-01-15 13:52:30 +0700

    Add list and detail routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'bulma';
 import './styles/main.css';
 import List from './pages/List';
+import Detail from './pages/Detail';
 
-const App = () => <List />;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={List} />
+      <Route path="/detail/:name" component={Detail} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
