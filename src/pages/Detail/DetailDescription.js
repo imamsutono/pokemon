bf1d2b8cdfb8b01e5b493ccad9fd9094ed254436
Stephen Daniel <stephendaniel60@gmail.com>
2021-01-15 14:02:30 +0700

    Optimize and fix frontend detail

--- src/pages/Detail/DetailDescription.js
+++ src/pages/Detail/DetailDescription.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
 const DetailDescription = ({data}) => {
-  const renderAbilities = () => {
+  const renderInlineList = (listKey, itemKey) => {
     let result = '';
-    data.abilities.map(({ability}) => result += `${ability.name} ∙ `);
-    return result;
-  };
-
-  const renderTypes = () => {
-    let result = '';
-    data.types.map(({type}) => result += `${type.name} ∙ `);
-    return result;
+    data[listKey].map((item) => result += `${item[itemKey].name} ∙ `);
+    return result.slice(0, -3);
   };
 
   const renderStats = () => (
     data.stats.map(({base_stat, stat}) => (
-      <div key={stat.name}>
+      <div key={stat.name} className="mb-2">
         <p>{stat.name} ({base_stat})</p>
         <progress className="progress is-warning" value={base_stat} max="100" />
       </div>
@@ -25,32 +19,36 @@ const DetailDescription = ({data}) => {
   return (
     <div className="column is-8">
       <p className="title is-size-1 mb-5">
-        {data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+        {`${data.name.charAt(0).toUpperCase()}${data.name.slice(1)}`}
       </p>
 
       <div className="columns">
         <div className="column">
           <strong className="has-text-grey">Types</strong>
-          <p className="is-size-5"><strong>{renderTypes()}</strong></p>
+          <p className="is-size-5">
+            <strong>{renderInlineList('types', 'type')}</strong>
+          </p>
         </div>
       </div>
       <div className="columns">
         <div className="column">
           <strong className="has-text-grey">Abilities</strong>
-          <p className="is-size-5"><strong>{renderAbilities()}</strong></p>
+          <p className="is-size-5">
+            <strong>{renderInlineList('abilities', 'ability')}</strong>
+          </p>
         </div>
       </div>
       <div className="columns">
         <div className="column is-2">
           <strong className="has-text-grey">Height</strong>
-          <p className="is-size-5"><strong>{`${data.height}"`}</strong></p>
+          <p className="is-size-5"><strong>{data.height}</strong></p>
         </div>
         <div className="column is-2">
           <strong className="has-text-grey">Weight</strong>
           <p className="is-size-5"><strong>{data.weight}</strong></p>
         </div>
       </div>
-      <div className="columns">
+      <div className="columns mb-4">
         <div className="column is-6">
           <strong className="has-text-grey">Base Stats</strong>
           <div className="mt-4"></div>
